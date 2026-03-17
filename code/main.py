703162179b5b61ead93438abcf1ec8aebc798502
Vojtěch Ožana <97116766+yeee32@users.noreply.github.com>
2026-03-17 17:39:19 +0100

    added stats tab

--- code/main.py
+++ code/main.py
@@ -5,6 +5,11 @@ import MultiListbox as table
 from data import data
 from tkinter import filedialog as fd
 from PIL import Image, ImageTk
+import matplotlib.pyplot as pp
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.ticker as ticker
+from datetime import datetime
 
 class App:
     def __init__(self, root):
@@ -33,6 +38,7 @@ class App:
         self.photo_path = StringVar()
         # Menu at top 
         self.top_menu = Menu(self.root)
+        
 
         # file
         self.file = Menu(self.top_menu, tearoff=0)
@@ -97,11 +103,11 @@ class App:
         self.detail_frame.rowconfigure(6, weight=1)     
 
         # details content
-        self.title_lbl = Label(self.detail_frame, text="Name", font=label_font)
-        self.date_lbl = Label(self.detail_frame, text="Date", font=label_font)
-        self.grade_lbl = Label(self.detail_frame, text="Grade", font=label_font)
-        self.place_lbl = Label(self.detail_frame, text="Place", font=label_font)
-        self.tries_lbl = Label(self.detail_frame, text="Tries", font=label_font)
+        self.title_label = Label(self.detail_frame, text="Name", font=label_font)
+        self.date_label = Label(self.detail_frame, text="Date", font=label_font)
+        self.grade_label = Label(self.detail_frame, text="Grade", font=label_font)
+        self.place_label = Label(self.detail_frame, text="Place", font=label_font)
+        self.tries_label = Label(self.detail_frame, text="Tries", font=label_font)
 
         self.title_entry = Entry(self.detail_frame, width=18, textvariable=self.route_name)
         self.date_entry = Entry(self.detail_frame, width=18, textvariable=self.date)
@@ -109,19 +115,19 @@ class App:
         self.place_entry = Entry(self.detail_frame, width=18, textvariable=self.place)
         self.tries_entry = Entry(self.detail_frame, width=18, textvariable=self.num_tries)
 
-        self.title_lbl.grid(row=0, column=0, sticky=W, padx=5, pady=2)
+        self.title_label.grid(row=0, column=0, sticky=W, padx=5, pady=2)
         self.title_entry.grid(row=0, column=1, sticky=EW, padx=5)
 
-        self.date_lbl.grid(row=1, column=0, sticky=W, padx=5, pady=2)
+        self.date_label.grid(row=1, column=0, sticky=W, padx=5, pady=2)
         self.date_entry.grid(row=1, column=1, sticky=EW, padx=5)
 
-        self.grade_lbl.grid(row=2, column=0, sticky=W, padx=5, pady=2)
+        self.grade_label.grid(row=2, column=0, sticky=W, padx=5, pady=2)
         self.grade_entry.grid(row=2, column=1, sticky=EW, padx=5)
 
-        self.place_lbl.grid(row=3, column=0, sticky=W, padx=5, pady=2)
+        self.place_label.grid(row=3, column=0, sticky=W, padx=5, pady=2)
         self.place_entry.grid(row=3, column=1, sticky=EW, padx=5)
 
-        self.tries_lbl.grid(row=4, column=0, sticky=W, padx=5, pady=2)
+        self.tries_label.grid(row=4, column=0, sticky=W, padx=5, pady=2)
         self.tries_entry.grid(row=4, column=1, sticky=EW, padx=5)
 
 
@@ -143,8 +149,6 @@ class App:
         self.detail_frame.rowconfigure(3, weight=1)
         self.detail_frame.rowconfigure(4, weight=1)
 
-        # Label(self.media_frame, text="Photo / Video").place(relx=0.5, rely=0.5, anchor=CENTER)
-
         # notes
         self.notes_text = Text(self.detail_frame, height=8)
         self.notes_text.grid(row=8, column=0, columnspan=3, sticky=NSEW, padx=5, pady=5)
@@ -165,15 +169,151 @@ class App:
         self.delete_button.pack(padx=5, pady = 5, side=LEFT, anchor=W, expand=True)
 
         # stats tab
-        ttk.Label(self.stats_tab, text="Stats tab").pack(fill=BOTH)
+        self.filter_label_frame = LabelFrame(self.stats_tab, text="Filter")
+        self.filter_label_frame.pack(fill=X, padx=10, pady=5)
+
+        self.filter_frame = Frame(self.filter_label_frame)
+        self.filter_frame.pack(fill=X, padx=5, pady=5)
+        
+        self.filter_place = Label(self.filter_label_frame, text="Place")
+        self.filter_place.pack(padx=5, pady=5, side=LEFT, anchor=CENTER)
+        self.place_entry = Entry(self.filter_label_frame)
+        self.place_entry.pack(padx=5, pady=5, side=LEFT, anchor=CENTER)
+
+        self.filter_button = Button(self.filter_label_frame, text="Search", width=10, height=1, command=self.filter)
+        self.filter_button.pack(padx=5, pady=5, side=RIGHT, anchor=CENTER)
+        
+        self.stats_frame = Frame(self.stats_tab)
+        self.stats_frame.pack(fill=BOTH, expand=True, padx=20, pady=20)
+
+        # holds the stats
+        self.stats_left = Frame(self.stats_frame)
+        self.stats_left.pack(side=LEFT, fill=BOTH, expand=True, padx=10)
+
+        # holds the graph
+        self.stats_right = Frame(self.stats_frame)
+        self.stats_right.pack(side=LEFT, fill=BOTH, expand=True, padx=20)
+        
+        def place_to_grid(row, label, entry):
+            label.grid(row=row, column=0, sticky=W, padx=5, pady=10)
+            entry.grid(row=row, column=1, sticky=EW, padx=5, pady=10)
+        
+        self.total_label = Label(self.stats_left, text="Total ascends", font=label_font)
+        self.flash_label = Label(self.stats_left, text="Flash count", font=label_font)
+        self.os_label = Label(self.stats_left, text="OS count", font=label_font)
+        self.hardest_label = Label(self.stats_left, text="Hardest grade", font=label_font)
+        self.avg_label = Label(self.stats_left, text="Average grade", font=label_font)
+        self.locations_label = Label(self.stats_left, text="Crag count", font=label_font)
+        
+        self.total_entry = Entry(self.stats_left)
+        self.flash_entry = Entry(self.stats_left)
+        self.os_entry = Entry(self.stats_left)
+        self.hardest_enry = Entry(self.stats_left)
+        self.avg_entry = Entry(self.stats_left)
+        self.locations_entry = Entry(self.stats_left)
+        
+        place_to_grid(0, self.total_label, self.total_entry)
+        place_to_grid(1, self.flash_label, self.flash_entry)
+        place_to_grid(2, self.os_label, self.os_entry)
+        place_to_grid(3, self.hardest_label, self.hardest_enry)
+        place_to_grid(4, self.avg_label, self.avg_entry)
+        place_to_grid(5, self.locations_label, self.locations_entry)
+        
+        # graph
+        
+        self.update_stats()
+        self.draw_histogram()
     
+    def draw_histogram(self):
+
+        grades = [
+            "5A","5A+","5B","5B+","5C","5C+",
+            "6A","6A+","6B","6B+","6C","6C+",
+            "7A","7A+","7B","7B+","7C","7C+",
+            "8A","8A+","8B","8B+","8C","8C+",
+            "9A","9A+","9B","9B+","9C"
+        ]
+
+        counts = {g:0 for g in grades}
+
+        for r in data:
+            grade = r.get("grade")
+            if grade in counts:
+                counts[grade] += 1
+
+        x = list(counts.keys())
+        y = list(counts.values())
+
+        fig, ax = pp.subplots(figsize=(6,4))
+        ax.bar(x, y)
+
+        ax.set_title("Climbs per Grade")
+        ax.set_xlabel("Grade")
+        ax.set_ylabel("Count")
+
+        ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+
+        ax.tick_params(axis="x", rotation=90)
+        ax.set_ylim(bottom=0)
+
+        for widget in self.stats_right.winfo_children():
+            widget.destroy()
+
+        canvas = FigureCanvasTkAgg(fig, master=self.stats_right)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill=BOTH, expand=True)
+        
+    def update_stats(self):
+        total = len(data)
+
+        flash = sum(1 for r in data if r.get("flash_var"))
+        os_count = sum(1 for r in data if r.get("os_var"))
+
+        grades = [r.get("grade") for r in data if r.get("grade")]
+        hardest = max(grades) if grades else "-"
+
+        locations = set(r.get("place") for r in data if r.get("place"))
+
+        self.total_entry.config(textvariable=total)
+        self.flash_entry.config(textvariable=flash)
+        self.os_entry.config(textvariable=os_count)
+        # self.avg_entry.config(textvariable=avg)
+        self.hardest_enry.config(textvariable=hardest)
+        self.locations_entry.config(textvariable=locations)
+        
+        self.total_entry.delete(0, END)
+        self.total_entry.insert(0, total)
+
+        self.flash_entry.delete(0, END)
+        self.flash_entry.insert(0, flash)
+
+        self.os_entry.delete(0, END)
+        self.os_entry.insert(0, os_count)
+
+        self.hardest_enry.delete(0, END)
+        self.hardest_enry.insert(0, hardest)
+
+        self.avg_entry.delete(0, END)
+        self.avg_entry.insert(0, "-")
+
+        self.locations_entry.delete(0, END)
+        self.locations_entry.insert(0, len(locations))
+        self.draw_histogram()
+        
     def new_options_window(self):
         print("Opened new options window")
+
+    def filter(self):
+        print("pressed filter button")
 
     def new_add_window(self):
         # reseting stuff
         self.row = None    
-        self.date.set("")
+        # automatically sets todays date
+        today = datetime.now()
+        ymd = "%Y-%m-%d"
+        date_today = today.strftime(ymd)
+        self.date.set(date_today)
         self.route_name.set("")
         self.grade.set("")
         self.place.set("")
@@ -210,6 +350,7 @@ class App:
         self.notes_text.delete("1.0", END)
         self.notes_text.insert(END, record.get("note"))
         
+        self.update_stats()
         self.show(self.row)
         self.edit_win.destroy()
         
@@ -260,14 +401,12 @@ class App:
             self.edit_win.geometry(f"{w}x{h}")
             self.edit_win.title("Route Details")
             
-            grades_french = ["5A","5A+","5B","5B+","5C","5C+",
+            grades = ["5A","5A+","5B","5B+","5C","5C+",
                             "6A","6A+","6B","6B+","6C","6C+",
                             "7A","7A+","7B","7B+","7C","7C+",
                             "8A","8A+","8B","8B+","8C","8C+",
                             "9A","9A+","9B","9B+","9C"]
             
-            grades_v = ["V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17"]
-                        
             self.main = Frame(self.edit_win)
             self.main.pack(fill=BOTH, expand=True)
 
@@ -285,7 +424,7 @@ class App:
 
             add_row(0, "Date", Entry(self.form, textvariable=self.date, width=25))
             add_row(1, "Name", Entry(self.form, textvariable=self.route_name, width=25))
-            add_row(2, "Grade", ttk.Combobox(self.form, textvariable=self.grade, values=grades_french, width=25))
+            add_row(2, "Grade", ttk.Combobox(self.form, textvariable=self.grade, values=grades, width=25))
             add_row(3, "Place", Entry(self.form, textvariable=self.place, width=25))
             add_row(4, "Tries", Spinbox(self.form, from_=1, to=100, textvariable=self.num_tries, width=5))
                         
@@ -336,7 +475,8 @@ class App:
         self.rp_var.set(False)
 
         self.notes_text.delete("1.0", END)
-
+        self.update_stats()
+    
     def upload_photo(self):
         file = fd.askopenfilename(parent=None, title="Choose your file", filetypes=[("all files", "*.*"), ("gif files", "*.gif"), ("png files", "*.png"), ("jpg files", "*.jpg")])
         if file:
@@ -351,7 +491,6 @@ class App:
                 
             Label(self.photo, image=self.photo_img).pack(expand=True)
             self.current_photo_path = file
-            # print("Photo loaded:", file_path)
 root = Tk()
 app = App(root)
 root.mainloop()
